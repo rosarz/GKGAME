dcfd15c6b41607543230f8ca1a06209d1ff0f8b5
rosarz <kanalcaspy@gmail.com>
2024-05-21 18:22:56 +0200

    update 0.2
    + dodałem jednego enemy przy dystansie 4 klatek zaczyna gonić playera jak znowu dystans bedzie >4 to przestaje go gonić (to wstępnie żeby było)
    
    + oprócz tego dodałem animacje idle do enemy
    
    +są kolizje wstawione między playerem a enemy i ze ścianami
    
    + jakoś basic ogarniam animacje do postaci

--- GKGAME/Assets/PlayerCtrl.cs
+++ GKGAME/Assets/PlayerCtrl.cs
@@ -7,19 +7,52 @@ public class PlayerCtrl : MonoBehaviour
 
     public float movSpeed; 
     float speedX, speedY;
-    Rigidbody2D rb; 
+    Rigidbody2D rb;
+    private Animator anim;
+
+    private bool faceLeft = false;
 
     // Start is called before the first frame update
     void Start()
     {
+        anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetAxis("Horizontal") > 0 && faceLeft)
+        {
+            Flip();
+            faceLeft = false;
+            anim.SetBool("isWalking", true);
+        }
+        if(Input.GetAxis("Horizontal") < 0 && !faceLeft)
+        {
+            Flip();
+            faceLeft = true;
+            anim.SetBool("isWalking", true);
+        }
+        else 
+        {
+            anim.SetBool("isWalking", false);
+        }
+
+        if(Input.GetMouseButton(0))
+        {
+            anim.SetTrigger("isAttacking");
+        }
+
         speedX = Input.GetAxis("Horizontal") * movSpeed;
         speedY = Input.GetAxis("Vertical") * movSpeed;
         rb.velocity = new Vector2(speedX, speedY);
+    }
+
+    private void Flip()
+    { 
+        var theScale = transform.localScale;
+        theScale .x *= -1;
+        transform.localScale = theScale;
     }
 }
